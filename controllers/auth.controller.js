ff2d0529c8f86a444bd11cfa5f4324544784fbe4
xavierms <xavierams08@gmail.com>
2022-03-05 14:34:50 -0400

    JSW

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,69 +1,68 @@
-const { response } = require('express');
-const  User  = require('../models/User');
-const bcrypt = require('bcryptjs');
+const { response } = require("express");
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const { generateJWT } = require("../helpers/jwt");
 
+const createUser = async (req, res = response) => {
+  console.log(req.body);
+  const { name, email, password } = req.body;
+  try {
+    // Verify E-Mail
+    const verifyEmail = await User.findOne({ email });
 
-
-const createUser = async(req,res = response)=>{
-   console.log(req.body);
-    const { name, email, password }= req.body;
-    try {
-        // Verify E-Mail
-        const verifyEmail = await User.findOne({ email});
-        
-        if (verifyEmail) {
-            return res.status(400).json({
-                ok:true,
-                msg: 'There is already a user with this email'
-            });
-        }
-         //Create user on model
-        const dbUser = new User( req.body );
-    
-        //Hashing password
-        const salt = bcrypt.genSaltSync();
-        dbUser.password = bcrypt.hashSync(password, salt);
-        //Generate JWT
-    
-        //Create user of db
-        await dbUser.save();
-
-        //Generar respuesta exitosa
-        return res.status(201).json({
-            ok:true,
-            uid: dbUser.id,
-            name,
-        });
- 
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            ok:false,
-            msg: 'Please talk with your admin'
-        });
+    if (verifyEmail) {
+      return res.status(400).json({
+        ok: true,
+        msg: "There is already a user with this email",
+      });
     }
-   
- 
-    
- 
+    //Create user on model
+    const dbUser = new User(req.body);
+
+    //Hashing password
+    const salt = bcrypt.genSaltSync();
+    dbUser.password = bcrypt.hashSync(password, salt);
+
+    //Generate JWT
+    const token = await generateJWT(dbUser.id, name);
+
+    //Create user of db
+    await dbUser.save();
+
+    //Generar respuesta exitosa
+    return res.status(201).json({
+      ok: true,
+      uid: dbUser.id,
+      name,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      ok: false,
+      msg: "Please talk with your admin",
+    });
+  }
 };
 
-const loginUser = (req,res  = response)=>{
+const loginUser = (req, res = response) => {
+  const { email, password } = req.body;
 
-   
-    const {email,password}= req.body;
-    
-    return res.json({
-        ok: true,
-        msg: 'Login user /'
+  try {
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      ok: false,
+      msg: "Please talk with your admin",
     });
+  }
 };
 
-const renewToken = (req,res  = response)=>{
-    return res.json({
-        ok: true,
-        msg: 'Renew'
-    });
+const renewToken = (req, res = response) => {
+  return res.json({
+    ok: true,
+    msg: "Renew",
+  });
 };
 
 //ES module
@@ -71,8 +70,8 @@ const renewToken = (req,res  = response)=>{
 //          createUser
 //     }
 
-module.exports={
-    createUser,
-    loginUser,
-    renewToken
-}
+module.exports = {
+  createUser,
+  loginUser,
+  renewToken,
+};
